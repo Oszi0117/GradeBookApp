85efb7aaad9301b275d9006b7e3c98f6dfc74a2e
KrystianKuska <47749622+OsheOse@users.noreply.github.com>
2022-03-05 18:59:40 +0100

    Add support for weighted GPAs
    @add-isweighted-to-basegradebook
    @refactor-to-support-isweighted
    @set-isweighted-property
    @add-weighted-to-createcommand

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -34,20 +34,29 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 3)
+            if (parts.Length != 4)
             {
-                Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
+                Console.WriteLine("Command not valid, Create requires a name, type of gradebook, if it's weighted (true / false).");
                 return;
             }
             var name = parts[1];
+            bool isWeighted = false;
+            if (parts[3] == "true")
+            {
+                isWeighted = true;
+            }
+            else if (parts[3] == "false")
+            {
+                isWeighted = false;
+            }
             BaseGradeBook gradeBook;
             if (parts[2] == "standard")
             {
-                gradeBook = new StandardGradeBook(name);
+                gradeBook = new StandardGradeBook(name, isWeighted);
             }
             else if (parts[2] == "ranked")
             {
-                gradeBook = new RankedGradeBook(name);
+                gradeBook = new RankedGradeBook(name, isWeighted);
             }
             else
             {
